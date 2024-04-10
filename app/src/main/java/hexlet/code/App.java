5cc514b314ac5503a98e8919138364343c0f13fe
Julia Lagunova <lagunaajul@gmail.com>
2024-04-10 18:38:38 +0300

    added picocli help

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,23 @@
 package hexlet.code;
 
+import picocli.CommandLine;
+
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        new CommandLine(new Differ()).execute("-h");
     }
 }
+
+@CommandLine.Command(
+        name = "gendiff", version = "1.0.0", mixinStandardHelpOptions = true,
+        description = "Compares two configuration files and shows a difference."
+)
+
+class Differ implements Runnable {
+    @Override
+    public void run() {
+        System.out.println("[gendiff]");
+    }
+}
+
