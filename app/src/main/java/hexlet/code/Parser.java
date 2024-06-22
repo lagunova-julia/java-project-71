24d08057d5811a1a40a7de28f0903826ab6546de
Julia <lagunaajul@gmail.com>
2024-06-22 14:18:22 +0300

    fixed App

--- app/src/main/java/hexlet/code/Parser.java
+++ app/src/main/java/hexlet/code/Parser.java
@@ -8,7 +8,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 public class Parser {
     public static Map<String, Object> getData(String content) throws Exception {
         ObjectMapper objectMapper = new ObjectMapper();
-        Map<String, Object> map = objectMapper.readValue(content, new TypeReference<>(){});
+        Map<String, Object> map = objectMapper.readValue(content, new TypeReference<>() { });
         return map;
     }
 }
